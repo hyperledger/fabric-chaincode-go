74b77c1e262e2fb0e6eafdfd973d109fed9523d8
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2024-06-01 13:32:33 +0100

    v2 implementation using fabric-protos-go-apiv2
    The v2 implementation retains wire-level compatibility with Fabric but
    does include the following breaking changes:
    
    - Chaincode implementation that make direct use of fabric-protos-go will
      experience protocol buffer namespace conflicts (as described in
      https://protobuf.dev/reference/go/faq/). Any use of fabric-protos-go
      must be replaced by fabric-protos-go-apiv2.
    - The Chaincode interface and Stub now use only pointers to protocol buffer
      messages in their API. Chaincode implementation must provide matching
      Init and Invoke methods.
    
    Signed-off-by: Mark S. Lewis <Mark.S.Lewis@outlook.com>

---
+++ v2/shim/internal/server_test.go
@@ -0,0 +1,59 @@
+/*
+Copyright State Street Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package internal_test
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/hyperledger/fabric-chaincode-go/v2/shim/internal"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/keepalive"
+)
+
+func TestBadServer(t *testing.T) {
+	srv := &internal.Server{}
+	err := srv.Start()
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "nil listener")
+
+	l, err := net.Listen("tcp", ":0") // #nosec G102
+	assert.NotNil(t, l)
+	assert.Nil(t, err)
+	srv = &internal.Server{Listener: l}
+	err = srv.Start()
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "nil server")
+}
+
+func TestServerAddressNotProvided(t *testing.T) {
+	kaOpts := &keepalive.ServerParameters{
+		Time:    1 * time.Minute,
+		Timeout: 20 * time.Second,
+	}
+	srv, err := internal.NewServer("", nil, kaOpts)
+	assert.Nil(t, srv)
+	assert.NotNil(t, err, "server listen address not provided")
+}
+
+func TestBadServerAddress(t *testing.T) {
+	kaOpts := &keepalive.ServerParameters{
+		Time:    1 * time.Minute,
+		Timeout: 20 * time.Second,
+	}
+	srv, err := internal.NewServer("__badhost__:0", nil, kaOpts)
+	assert.Nil(t, srv)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "listen tcp: lookup __badhost__")
+
+	srv, err = internal.NewServer("host", nil, kaOpts)
+	assert.Nil(t, srv)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "listen tcp: address host: missing port in address")
+}
